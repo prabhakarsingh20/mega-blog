7c6b386d6bc2ba64bfc97f698589142642a8c142
prabhakarsingh20 <prabhakarsingh3333@gmail.com>
2025-10-10 01:04:35 +0530

    update the post form

---
+++ src/components/post-form/PostForm.jsx
@@ -0,0 +1,9 @@
+import React,{ useCallback } from 'react'
+import { useForm } from 'react-hook-form'
+import {Button, Input, RTE, Select} from "..";
+
+export default function PostForm() {
+  return (
+    <div>PostForm</div>
+  )
+}
