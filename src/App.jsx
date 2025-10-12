be6a12ed62865e6c21e1ef9d39d230d8b2346a09
prabhakarsingh20 <prabhakarsingh3333@gmail.com>
2025-10-13 00:33:31 +0530

    Create reusable Button.jsx and Input.jsx components for form elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import './App.css'
-import authService from "./appwrite/auth"
+import authService from "./service-appwrite/auth"
 import {login, logout} from "./store/authSlice"
 import { Footer, Header } from './components'
 import { Outlet } from 'react-router-dom'
--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
-import authService from '../../appwrite/config'
+import authService from '../../service-appwrite/config'
 import {logout} from '../../store/authSlice'
 
 export default function LogoutBtn() {
