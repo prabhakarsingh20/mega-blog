93f4a1f5333e567063dcc24e12fccd25598ddeb3
prabhakarsingh20 <prabhakarsingh3333@gmail.com>
2025-10-19 23:39:56 +0530

    create common button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Button({
     children, 
     type='button',
-    bgColor='bg=blue-600',
-    textColor="text-white",
-    className='',
+    bgColor='blue-blue-600',
+    textColor='text-white',
+    className="",
     ...props
 }) {
   return (
